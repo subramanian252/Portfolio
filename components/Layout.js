8bdd06caf39e21d6e365d2dbd6000495bbbeab13
subramanian m <subramanian@subramanians-MacBook-Pro.local>
2024-06-11 01:11:38 +0530

    third commit

--- components/Layout.js
+++ components/Layout.js
@@ -7,6 +7,7 @@ const sora = Sora({
   subsets: ["latin"],
   variable: "--font-sora",
   weight: ["100", "200", "300", "400", "500", "600", "700"],
+  display: "swap",
 });
 
 const Layout = ({ children }) => {
